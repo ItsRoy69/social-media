3de4d19125e96e5e593d68fde02cc9f0f5c74e18
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-18 21:40:29 +0530

    Connecting to database

--- server/index.js
+++ server/index.js
@@ -3,4 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const app = express();
 
+app.use (bodyParser.json({ limit: "30mb", extended: true }));
+app.use (bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mysocialmediadata:123456roy@cluster0.9lkaq.mongodb.net/social-media?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
