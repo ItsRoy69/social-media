0c5e326c3d120c629df6ce35e24f592d02d25df0
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-19 13:54:42 +0530

    setting up dispatch to call posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import memories from './images/memories.png';
@@ -8,6 +10,12 @@ import useStyles from './styles.js';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
 
   return (
       <Container maxWidth="lg">
