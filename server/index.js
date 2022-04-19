b1d84738e2f26506eba09a21a4d54e480e78e6e0
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-19 14:14:48 +0530

    calling

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use (bodyParser.json({ limit: "30mb", extended: true }));
 app.use (bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://mysocialmediadata:123456roy@cluster0.9lkaq.mongodb.net/social-media?retryWrites=true&w=majority"
 
