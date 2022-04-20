327ef8c43e01e66f85d5e53c09d4e698af6cc283
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-20 07:55:05 +0530

    ThemeProvide, useState

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@mui/material';
+
+import useStyles from './styles';
+
 
 const Form = () => {
+  const [postData, setPostData] = useState({ title: '', creator: '', message: '', selectedFile: ''});
+  const classes = useStyles();
 
+  const handleSubmit = () => {
+
+  }
   return (
-    <div>
-      form
-    </div>
+    <>
+      <Paper className={classes.paper}>
+        <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant='h6'> Creating a Memory</Typography>
+            <TextField
+              name='creator' 
+              label='Creator'
+              variant='outlined'
+              fullWidth
+              value={postData.creator}
+              onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+             />
+        </form>
+      </Paper>
+    </>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,20 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import {createTheme} from '@mui/material';
+import {ThemeProvider} from '@mui/styles';
 
 import App from './App';
 import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const theme = createTheme()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('root')
 );
