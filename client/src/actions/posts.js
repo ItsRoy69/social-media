3ba67499cfbaad49466f09cd8dced750080b86bf
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-27 22:32:35 +0530

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -13,7 +13,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await api.createPost();
+        const { data } = await api.createPost(post);
 
         dispatch( { type: 'CREATE', payload: data } );
     } catch (error) {
