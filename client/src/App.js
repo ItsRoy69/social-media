47691ac3c33c7aa367cc221bf66d085f64cd1583
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-05-02 23:15:01 +0530

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Container} from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar.js';
 import Home from './components/Home/Home.js';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-  const dispatch = useDispatch();
 
   return (
-      <Container maxWidth="lg">
-        <Navbar />
-        <Home />
-      </Container>
+        <Container maxWidth="lg">
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/auth" exact element={<Auth />} />
+          </Routes>
+        </Container>
   );
 }
 
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+
+const Auth = () => {
+    const state = null;
+    
+    return (
+        <>
+        AUTH
+        </>
+    )
+}
+
+export default Auth
