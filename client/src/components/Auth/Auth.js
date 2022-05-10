00ec50574c399e8dcf6aa3276bbd203948af7523
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-05-10 06:13:28 +0530

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@mui/material';
+
+import useStyles from './styles.js';
 
 const Auth = () => {
     const state = null;
